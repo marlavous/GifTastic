f260b44c966147ec2b5ceb9edf35603dc5c722ef
Mar <marcianelli.m@att.net>
2017-03-08 18:41:04 -0800

    edits

--- logic.js
+++ logic.js
@@ -1,9 +1,8 @@
+$(document).ready(function() {
+
 //Initial array of characters
     var characters = ["Mickey Mouse", "Belle", "Nemo", "The Aristocats", "Steamboat Willie", "Minnie Mouse", "Ariel", "Olaf Disney", "Donald Duck", "Pluto Disney", "Buzz Lightyear", "Ligntning McQueen", "Chewbacca", "Alladin", "Han Solo", "Genie in Alladin"];
 
-    
-      
-    
 
     //displays the GIFs on the html. limited to 10 
   	$("button").on("click",function(){
@@ -28,8 +27,22 @@
     		castImg.attr("src", results[i].images.fixed_height.url);
     		castDiv.append(p);
     		castDiv.append(castImg);
+            castImg.addClass("play");
 
     		$("#gif-view").prepend(castDiv);
+
+            $(".play").on("click", function(event){
+                var dataAnimate = $(this).attr("data-animate");
+                var dataStill = $(this).attr("data-still");
+                var currentURL = $(this).attr("src");
+                if( currentURL === dataStill){
+                    $(this).attr("src", dataAnimate);
+                } else {
+                    $(this).attr("src", dataStill)
+                }
+            })
+
+
 
     	}
 
@@ -37,5 +50,7 @@
     	});
 
       });
+
+})
 
 		
