d90b95549fb40355f0e3a84d08d7e13601b76cc0
Mar <marcianelli.m@att.net>
2017-03-09 10:27:53 -0800

    edits

--- logic.js
+++ logic.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
 //Initial array of characters
-    var characters = ["Mickey Mouse", "Belle", "Nemo", "The Aristocats", "Steamboat Willie", "Minnie Mouse", "Ariel", "Olaf Disney", "Donald Duck", "Pluto Disney", "Buzz Lightyear", "Ligntning McQueen", "Chewbacca", "Alladin", "Han Solo", "Genie in Alladin"];
+    var characters = ["Mickey Mouse", "Belle", "Nemo", "The Aristocats", "Steamboat Willie", "Minnie Mouse", "Ariel", "Olaf Frozen", "Donald Duck", "Pluto Disney", "Buzz Lightyear", "cars Mater", "Chewbacca", "Alladin", "Han Solo", "Genie in Alladin"];
 
 
     //displays the GIFs on the html. limited to 10 
-  	$("button").on("click",function(){
+  	// $("button").on("click",function(){
+    function displayCharacters(){
 
   		var cast = $(this).attr("data-name");
     	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cast + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -41,16 +42,41 @@ $(document).ready(function() {
                     $(this).attr("src", dataStill)
                 }
             })
-
-
-
     	}
-
-
     	});
+      };
 
-      });
+    function makeButtons(){
+
+        $("#buttonlocation").empty();
+
+        for (var i = 0; i < characters.length; i++){
+            var a = $("<button>");
+            a.addClass("cast");
+            a.attr("data-name", characters[i]);
+            a.text(characters[i]);
+            $("#buttonlocation").append(a);
+
+        }
+    }
+
+    $("#addButton").on("click", function(event){
+        event.preventDefault();
+        var cast = $("addCast").val().trim();
+        characters.push(cast);
+        makeButtons();
+    })
+
+    $(document).on("click", ".cast", displayCharacters)
+
+    makeButtons();
 
 })
+
+
+
+
+
+
 
 		
