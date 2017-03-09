f46d618405543447088f86c181e5636a70c8fe54
Mar <marcianelli.m@att.net>
2017-03-08 17:39:59 -0800

    work

---
+++ logic.js
@@ -0,0 +1,41 @@
+//Initial array of characters
+    var characters = ["Mickey Mouse", "Belle", "Nemo", "The Aristocats", "Steamboat Willie", "Minnie Mouse", "Ariel", "Olaf Disney", "Donald Duck", "Pluto Disney", "Buzz Lightyear", "Ligntning McQueen", "Chewbacca", "Alladin", "Han Solo", "Genie in Alladin"];
+
+    
+      
+    
+
+    //displays the GIFs on the html. limited to 10 
+  	$("button").on("click",function(){
+
+  		var cast = $(this).attr("data-name");
+    	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cast + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    	$.ajax({
+    		url:queryURL,
+    		method: "get"
+    	}).done(function(response){
+
+    	var results = response.data;   //accesses info inside data key
+
+    	for (var i = 0; i < results.length; i++){
+    		var castDiv = $("<div class = 'castDiv'>")  //create a new div
+
+    		var p = $("<p>").text("Rating: " + rating)  //put rating on page
+    		var rating = results[i].rating;             //this is where the rating info is located
+
+    		var castImg = $("<img>");
+    		castImg.attr("src", results[i].images.fixed_height.url);
+    		castDiv.append(p);
+    		castDiv.append(castImg);
+
+    		$("#gif-view").prepend(castDiv);
+
+    	}
+
+
+    	});
+
+      });
+
+		
